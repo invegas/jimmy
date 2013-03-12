4fdffe31b5115970250f0386aea4efbdc81f9470
liguangyi <liguangyi@liguangyi.local>
2013-03-12 18:53:48 +0800

    add class

---
+++ js/paraell.js
@@ -0,0 +1,45 @@
+window.parael = (function () {
+	var progress = 0, total, innerWidth, person, win, offsetx;
+
+	var scrollInit = function () {
+		// $(window).scrollLeft(0);
+		document.location.href = "#";
+	}	
+
+	var initSetting = function (cfg) {
+		win = cfg.win;
+		person = cfg.person;
+		total = cfg.total;
+		innerWidth = $(window).innerWidth();
+	}
+
+	var init = function (cfg) {
+
+		scrollInit();
+		initSetting(cfg)
+		walk();
+
+		win.scroll(function () {
+			onProgress();
+			walk();
+		}).resize(function () {
+			innerWidth = win.innerWidth();
+			onProgress();
+			walk();
+		})
+	}
+
+	var walk = function () {
+		person.css('left', (innerWidth - 50) * (1 - progress) + "px");
+	}
+
+	var onProgress = function () {
+	    offsetx = win.scrollLeft();
+	    progress = offsetx / (total - win.innerWidth());
+	    console.log(progress);    		
+	}
+
+	return {
+		init: init
+	}
+})()
