651401168dba06c395bf11c4a69797a3442bd178
liguangyi <liguangyi@liguangyi.local>
2013-03-13 19:30:42 +0800

    add timline

--- js/parallax.js
+++ js/parallax.js
@@ -1,11 +1,20 @@
 window.Parallax = (function () {
-	var progress = 0, total, innerWidth, person, person_w, win, offsetx;
+	var progress = 0.0, total, innerWidth, person, person_w, win, offsetx;
 	var level = 0;
+	var timeline = {
+		0.0: {
+			skyColor: "6b92b9"
+		},
+		0.1: {
+			sayWord: "It's cold here, and It's going to dark"
+		},
+		0.2: {
+			skyColor: "black",
+			sayWord: "Look, just as I say"			
+		}
+	}
 
 	var scrollInit = function () {
-		// window.scrollTo(0, 0);
-		// $(window).scrollLeft(0);
-		// document.location.href = "#";
 		setTimeout (function () {
 			scrollTo(0,0);
 		}, 100);
@@ -27,66 +36,102 @@ window.Parallax = (function () {
 
 		win.scroll(function () {
 			onProgress();
-			walk();
+			// walk();
 		}).resize(function () {
 			innerWidth = win.innerWidth();
 			onProgress();
-			walk();
+			// walk();
 		})
 	}
 
-	var speak = function () {
-		var shutup = function () {
-			$('.speech').removeClass('is-say');
-		}
+	// var speak = function () {
+	// 	var shutup = function () {
+	// 		$('.speech').removeClass('is-say');
+	// 	}
 
-		var say = function (word) {
-			$('.speech').addClass('is-say');
-			console.log(word);
-		}
+	// 	var say = function (word) {
+	// 		$('.speech').addClass('is-say');
+	// 		$('.speech-content').text(word);
+	// 	}
 
-		var percent = progress.toFixed(1);
-		console.log(percent);
+	// 	var percent = progress.toFixed(1);
 
-		switch(percent) {
-			case "0.1":  {
-				if (level === 1) return;
-				say("now begin");
-				level = 1;
-				break;
-			}
-			case "0.3": {
-				if (level === 3) return;
-				say("on progress"); 
-				level = 3;
-				break;
-			}
-			case "0.5": {
-				if (level === 5) return;
-				say("it's cold here"); 
-				level = 5;
-				break;
-			}
-			default {
-				if (level === 0) return;
-				shutup();
-				level = 0;
-			}
-		}
-
-
-	}
+	// 	switch(percent) {
+	// 		case "0.1":  {
+	// 			if (level === 1) return;
+	// 			say("now begin");
+	// 			level = 1;
+	// 			break;
+	// 		}
+	// 		case "0.3": {
+	// 			if (level === 3) return;
+	// 			say("on progress"); 
+	// 			$('body').css('background-color', 'black');
+	// 			level = 3;
+	// 			break;
+	// 		}
+	// 		case "0.5": {
+	// 			if (level === 5) return;
+	// 			say("it's cold here"); 
+	// 			level = 5;
+	// 			break;
+	// 		}
+	// 		default: {
+	// 			if (level === 0) return;
+	// 			shutup();
+	// 			level = 0;
+	// 			$('body').css('background-color', '#6b92b9');
+	// 		}
+	// 	}
+	// }
 
 	var walk = function () {
 		person.css('left', (innerWidth - person_w) * (1 - progress) + "px");
-		speak();
 		// $('.bg-item').css('background-position-x', '+=10px');
+	}
+
+	var speak = function (word) {
+		$('.speech').addClass('is-say');
+		$('.speech-content').text(word);		
+	}
+
+	var shutup = function () {
+		$('.speech').removeClass('is-say');		
+	}
+
+	var skyColorChange = function (color) {
+		if (!color) var color = '#6b92b9';
+		$('body').css('background-color', color);
 	}
 
 	var onProgress = function () {
 	    offsetx = win.scrollLeft();
 	    progress = offsetx / (total - win.innerWidth());
-	    // console.log(progress);    		
+	    console.log(progress);
+
+	    // Jimmy walk
+	    walk(); 
+	    // I need a bus !!!
+
+	    // Weather
+	    var percent = progress.toFixed(1);
+	    if (timeline[percent]) {
+	    	var cfg = timeline[percent];
+	    	// sky color
+	    	if (cfg.skyColor) {
+	    		$('body').css('background-color', cfg.skyColor);
+	    	}
+
+	    	// say something
+	    	if (cfg.sayWord) {
+	    		$('.speech').addClass('is-say');
+				$('.speech-content').text(cfg.sayWord);
+	    	}
+	    } else {
+	    	shutup();
+	    	skyColorChange();
+	    }
+
 	}
 
 	return {
