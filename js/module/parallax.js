a329dfccbd93ebf2816f742b80839f158be9bbb7
liguangyi <liguangyi@liguangyi.(none)>
2013-03-14 14:13:30 +0800

    complee

--- js/module/parallax.js
+++ js/module/parallax.js
@@ -1,27 +1,42 @@
 window.Parallax = (function () {
-	var progress = 0.0, total, innerWidth, person, person_w, win, offsetx;
+	var progress = 0.0, total, innerWidth, person, person_w, win, offsetx, light;
 	var level = 0;
+	var memory = 1000;
+
 	var timeline = {
+		0.1: {
+			sayWord:"It's cold here, and it's going to dark, I need move on."
+		},
 		0.2: {
-			skyColor: "#5e879b"		
+			skyColor: "#032435",
+			light: 0.2
 		},
 		0.3: {
-			skyColor: "#41677a"
+			skyColor: "#032435",
+			light: 0.7,
+			sayWord: "It remind me ofsomething...the first time I met her."
 		},
 		0.4: {
-			skyColor: "#224757"
+			skyColor: "#032435",
+			light: 1
 		},
 		0.5: {
-			skyColor: "#032435"
+			skyColor: "#032435",
+			light: 1,
+			sayWord: "However...everything is gone."
 		},
 		0.6: {
-			skyColor: "#224757"
+			skyColor: "#032435",
+			light: 1
 		},
 		0.7: {
-			skyColor: "#41677a"
+			skyColor: "#41677a",
+			light: 0.7,
+			sayWord: "......"
 		},
 		0.8: {
-			skyColor: "#5e879b"
+			skyColor: "#5e879b",
+			light: 0.2
 		}							
 
 	}
@@ -35,6 +50,7 @@ window.Parallax = (function () {
 	var initSetting = function (cfg) {
 		win = cfg.win;
 		person = cfg.person;
+		light = cfg.light;
 		person_w = parseInt(person.css('width'));
 		total = cfg.total;
 		innerWidth = $(window).innerWidth();
@@ -48,57 +64,16 @@ window.Parallax = (function () {
 
 		win.scroll(function () {
 			onProgress();
-			// walk();
 		}).resize(function () {
 			innerWidth = win.innerWidth();
 			onProgress();
-			// walk();
 		})
 	}
 
-	// var speak = function () {
-	// 	var shutup = function () {
-	// 		$('.speech').removeClass('is-say');
-	// 	}
-
-	// 	var say = function (word) {
-	// 		$('.speech').addClass('is-say');
-	// 		$('.speech-content').text(word);
-	// 	}
-
-	// 	var percent = progress.toFixed(1);
-
-	// 	switch(percent) {
-	// 		case "0.1":  {
-	// 			if (level === 1) return;
-	// 			say("now begin");
-	// 			level = 1;
-	// 			break;
-	// 		}
-	// 		case "0.3": {
-	// 			if (level === 3) return;
-	// 			say("on progress"); 
-	// 			$('body').css('background-color', 'black');
-	// 			level = 3;
-	// 			break;
-	// 		}
-	// 		case "0.5": {
-	// 			if (level === 5) return;
-	// 			say("it's cold here"); 
-	// 			level = 5;
-	// 			break;
-	// 		}
-	// 		default: {
-	// 			if (level === 0) return;
-	// 			shutup();
-	// 			level = 0;
-	// 			$('body').css('background-color', '#6b92b9');
-	// 		}
-	// 	}
-	// }
 
 	var walk = function () {
 		person.css('left', (innerWidth - person_w) * (1 - progress) + "px");
+		light.css('left', innerWidth * progress + "px");
 	}
 
 	var speak = function (word) {
@@ -116,28 +91,43 @@ window.Parallax = (function () {
 	}
 
 	var bgPull = function () {
-// $('.bg-item').css('background-position-x', '+=10px');
+		var percent;
+	    // 回忆是否进入
+	    if (offsetx > 3000) {
+
+	    } else if (2000 - offsetx - innerWidth < 0) {
+	    	// 在其中时
+	    	// 2000 - offsetx: 在 part3 中出现在屏幕中的区域
+	    	percent = 2000 - offsetx - innerWidth;
+	    	percent = Math.abs(percent);
+	    	percent = percent / innerWidth;
+			
+	    } else {
+	    	// 即将接近时
+ 	    	percent = (offsetx + innerWidth) / 2000;
+ 	    	percent = 1 - percent
+ 	    }	
+
+ 	    $('.bg-parallax').css('background-position-x', percent * 100 + "%");	    		
+	}
+
+	var lighting = function (bright) {
+		$('.light-bulb').css('background-color', 'rgba(255, 255, 0, ' + bright + ')');
 	}
 
 	var onProgress = function () {
 	    offsetx = win.scrollLeft();
 	    var origin = progress;
 	    progress = offsetx / (total - win.innerWidth());
-	    // $('.bg-item').css('background-position-x', (1 - progress) * 2000);
 
-	    if (progress > origin) {
-	    	$('.bg-item').css('background-position-x', '-=30px');
-	    } else {
-	    	$('.bg-item').css('background-position-x', '+=30px');
-	    }
+	    bgPull();
 
 	    // Jimmy walk
 	    walk(); 
-	    // I need a bus !!!
 
 	    // Weather
 	    var percent = Util.myFixed(progress, 1);
-	    console.log(percent);
+	    // console.log(percent);
 	    if (timeline[percent]) {
 	    	var cfg = timeline[percent];
 	    	// sky color
@@ -149,7 +139,12 @@ window.Parallax = (function () {
 	    	if (cfg.sayWord) {
 	    		speak(cfg.sayWord);
 	    	} else {
+	    		shutup();
+	    	}
 
+	    	// change the street light
+	    	if (cfg.light) {
+	    		lighting(cfg.light);
 	    	}
 
 	    } else {
